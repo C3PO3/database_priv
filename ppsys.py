6c0ed48dba4f1125806c15a9b1617b3b0670d9fa
Sara Alam <sara.alam@tufts.edu>
2024-04-13 15:19:42 -0400

    hello2

---
+++ ppsys.py
@@ -0,0 +1,28 @@
+import psycopg2
+'''
+Assume there is a list of queries.
+1. Connect to a postgres server
+2. For each query, run it without noise.
+3. For each query, run it with noise 10 times with the same pre-determined privacy budget.
+'''
+def main():
+    #establishing the connection
+    conn = psycopg2.connect(
+    database="cc151", user='postgres', password='1908', host='127.0.0.1', port= '5433'
+    )
+    #Creating a cursor object using the cursor() method
+    cursor = conn.cursor()
+
+    #Executing an MYSQL function using the execute() method
+    cursor.execute("select version()")
+
+    # Fetch a single row using fetchone() method.
+    data = cursor.fetchone()
+    print("Connection established to: ",data)
+
+    #Closing the connection
+    conn.close()
+    
+if __name__=='__main__':
+    main()
+    
